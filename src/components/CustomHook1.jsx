cc97efeb6a0f4ad7b0316e3b6a03a882230b92ef
TrungNguyen1409 <trungnguyenb0k30@gmail.com>
2022-11-16 13:10:28 +0100

    this could be the solutoin for the project

--- src/components/CustomHook1.jsx
+++ src/components/CustomHook1.jsx
@@ -3,19 +3,41 @@ import useLocalStorage from '../hooks/UseLocalStorage'
 function CustomHook1() {
 
     const [task,setTask] = useLocalStorage('task','')
+    const [tasks, setTasks] = useLocalStorage('tasks', [])
+    
+    const onSubmit = e =>{
+        e.preventDefault()
+        console.log(123)
+        const taskObj = {
+            task,
+            completed: false,
+            date: new Date().toLocaleDateString()
+        }
 
+        setTasks([...tasks, taskObj])
+    }
+ 
+ 
   return (
     <div>
-        <form className='w-50'> 
+        <form onSubmit={onSubmit} className='w-50'> 
             <div className='mb-3'>
                 <label className='form-label'>Task</label>
                 <input 
                     className='form-control'
                     type="text" 
                     value={task} 
-                    onChange={(e)=> setTask(e.target.value)}/>
+                    onChange={(e)=> setTask(e.target.value)}
+                />
+
             </div>
+             <button type='submit' className='btn btn-primary'> Submit</button>
+
         </form>
+        <hr/>
+        {tasks.map((task)=>(<h3 key={task.date}>
+            {task.task}
+        </h3>))}
     </div>
   )
 }
